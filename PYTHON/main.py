6c4503239c1d25a7003f7b46e61632d84e094a8b
Rastelli <francescorastelli2005@gmail.com>
2022-07-30 21:46:50 +0200

    PYTHON try

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,6 +1,17 @@
+from operator import mod
+from webbrowser import get
+from yaml import scan
+
+
 print("Hello World!") #!come stampare
 fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   print(x)
 for i in range (0,10):
   print(i)
+  if (i%2==0):
+    print("pari")
+  else:
+    print("dispari")
+a = 1
+#TODO while(a!=0)
